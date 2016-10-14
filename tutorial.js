f238e51cb43f0814097c3cd4a12b57156d833c1e
Tom Constantine <thomas.constantine@jobsite.co.uk>
2016-10-14 14:39:56 +0100

    Adding server code

---
+++ tutorial.js
@@ -0,0 +1,12 @@
+'use strict'
+let restify = require('restify');
+let server = restify.createServer({});
+
+server.get('/', (req, res, next) => {
+  res.send(200, 'Debug App');
+  return next();
+});
+
+server.listen(2000, () => {
+  console.log('listening at port 2000');
+});
